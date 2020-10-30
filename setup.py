d0cecced3e2b24652d542bc0f7ed255f4da61b54
gomachssm <gomach.ssm@gmail.com>
2020-10-30 16:14:02 +0900

    パッケージ情報の取得方法変更、ローカルで実行する場合、sdistなどで作成されたディレクトリを事前に削除するロジックを追加

--- setup.py
+++ setup.py
@@ -2,29 +2,50 @@
 
 import os
 import setuptools
-
-import twsqlparser
+import shutil
 
 PKG_NAME = 'twsqlparser'
+here = os.path.abspath(os.path.dirname(__file__))
 
 
 def abspath(path):
     return os.path.abspath(os.path.join(os.path.dirname(__file__), path))
 
 
-def load_md(path):
+def delete_dir(path_list: list):
+    for path in path_list:
+        path = abspath(path)
+        if not os.path.isdir(path):
+            print(f'[DEBUG] Directory `{path}` is not exist.')
+            continue
+        shutil.rmtree(path)
+        print(f'[DEBUG] Succeed, Directory `{path}` is deleted.')
+
+
+def load_md(path: str):
     with open(path, 'r', encoding='UTF-8') as f:
         return f.read()
 
 
+def load_env_by_exec_py(path: str):
+    env_map = {}
+    python_file_path = abspath(path)
+    with open(python_file_path, mode='r', encoding='utf-8') as f:
+        exec(f.read(), env_map)
+        return env_map
+
+
 def setup_pkg():
+    delete_dir(['build', 'dist', 'twsqlparser.egg-info'])
+    about = load_env_by_exec_py('twsqlparser/__pkg_info__.py')
     setuptools.setup(
         name=PKG_NAME,
         packages=[PKG_NAME],
-        version=twsqlparser.__version__,
-        license=twsqlparser.__license__,
-        author=twsqlparser.__author__,
-        url=twsqlparser.__url__,
+        version=about['__version__'],
+        license=about['__license__'],
+        author=about['__author__'],
+        url=about['__url__'],
+        package_dir={PKG_NAME: PKG_NAME},
         description='2way SQL parser for Python',
         long_description=load_md(abspath('./README.md')),
         long_description_content_type='text/markdown',
