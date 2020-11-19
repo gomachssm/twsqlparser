5815e9c7be9baf87b02fa7907d708f192468a38c
gomachssm <gomach.ssm@gmail.com>
2020-11-20 08:45:39 +0900

    add sample useing pg8000

--- sample/postgres/speed_check.py
+++ sample/postgres/speed_check.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 # (C) 2020 gomachssm
 
-import datetime
 import os
+import sys
+import datetime
 import pg8000
 import twsqlparser as twsp
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 pg8000.paramstyle = 'named'
 
 
 def main():
-    conn = pg8000.connect(user='postgres', password='password', database='time')
+    from .PG8000Con import PG8000Con
     loop = 50
     methods = [twspsimpleinsert, simple_insert, twspmultiinsert, exec_many]
-    for mthd in methods:
-        mthd(loop, conn)
-    for mthd in reversed(methods):
-        mthd(loop, conn)
-    conn.close()
+    timemap = {}
+    with PG8000Con(host='localhost', port=5432, user='postgres', password='password', database='postgres') as conn:
+        for mthd in [*methods, *(reversed(methods))]:
+            msec = mthd(loop, conn)
+            timemap[mthd.__name__] = timemap.get(mthd.__name__, 0) + msec
+    print('====================')
+    for k, v in timemap.items():
+        print(f'{k:<16}\t{v}')
 
 
 def twspmultiinsert(loop: int, conn):
@@ -29,9 +35,11 @@ def twspmultiinsert(loop: int, conn):
         params = {'names': ['C', 'D', 'F#', 'Go', 'KQ']}
         sql, exec_param = twsp.parse_file(path, params)
         cur.execute(sql, exec_param)
-        conn.commit()
     end = datetime.datetime.now().timestamp()
-    print(f'twspmultiinsert: {end - start}')
+    conn.commit()
+    msec = end - start
+    print(f'twspmultiinsert: {msec}')
+    return msec
 
 
 def twspsimpleinsert(loop: int, conn):
@@ -44,9 +52,11 @@ def twspsimpleinsert(loop: int, conn):
         for name in names:
             sql, exec_param = twsp.parse_sql(base_sql, {'name': name})
             cur.execute(sql, {'name': name})
-        conn.commit()
     end = datetime.datetime.now().timestamp()
-    print(f'twspsimpleinsert: {end - start}')
+    conn.commit()
+    msec = end - start
+    print(f'twspsimpleinsert: {msec}')
+    return msec
 
 
 def simple_insert(loop: int, conn):
@@ -58,9 +68,11 @@ def simple_insert(loop: int, conn):
         sql = 'insert into sample_lang (name) values ( :name );'
         for name in names:
             cur.execute(sql, {'name': name})
-        conn.commit()
     end = datetime.datetime.now().timestamp()
-    print(f'simple_insert: {end - start}')
+    conn.commit()
+    msec = end - start
+    print(f'simple_insert: {msec}')
+    return msec
 
 
 def exec_many(loop: int, conn):
@@ -72,9 +84,11 @@ def exec_many(loop: int, conn):
         sql = 'insert into sample_lang (name) values ( :name );'
         params = ({'name': name} for name in names)
         cur.executemany(sql, params)
-        conn.commit()
     end = datetime.datetime.now().timestamp()
-    print(f'executemany: {end - start}')
+    conn.commit()
+    msec = end - start
+    print(f'executemany: {msec}')
+    return msec
 
 
 if __name__ == '__main__':
