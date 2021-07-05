9f2e98e6e0618b464b054bc914bddd309c78ded0
gomachssm <gomach.ssm@gmail.com>
2021-07-05 23:25:59 +0900

    fix import error

--- twsqlparser/__init__.py
+++ twsqlparser/__init__.py
@@ -3,6 +3,6 @@
 
 from . import internal_exceptions
 from .twsp import parse_sql, parse_file
-from .__pkg_info__ import __author__, __copyright__, __license__, __url__, __version__  # noqa: F401
+from .__pkg_info__ import __author__, __copyright__, __license__, __url__   # noqa: F401
 
 __all__ = ['parse_sql', 'parse_file', 'internal_exceptions']
