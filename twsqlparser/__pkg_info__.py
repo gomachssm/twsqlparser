83a786d5a37396a09875e6f39a082defc6fb7f31
gomachssm <gomach.ssm@gmail.com>
2021-07-05 23:28:41 +0900

    fix version error

--- twsqlparser/__pkg_info__.py
+++ twsqlparser/__pkg_info__.py
@@ -4,7 +4,7 @@
 import datetime
 
 __copyright__ = f'(C) {datetime.date.today().year} gomachssm'
-# __version__ = 'X.X.X'       # get from tag, matches v([0-9]+\.[0-9]+\.[0-9]+).
+__version__ = 'dummy'   # get from tag, matches v([0-9]+\.[0-9]+\.[0-9]+).
 __license__ = 'Apache License, Version 2.0'
 __author__ = 'gomachssm'
 __url__ = 'https://github.com/gomachssm/twsqlparser'
